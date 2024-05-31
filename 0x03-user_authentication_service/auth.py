a703de1fa25127de96a9bc76a3dcb213d9273f6e
Abdeljalil37 <widadpop@gmail.com>
2024-05-31 02:32:06 +0100

    Add _hash_password function for password hashing

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Definition of _hash_password function
+"""
+import bcrypt
+from uuid import uuid4
+from sqlalchemy.orm.exc import NoResultFound
+from typing import (
+    TypeVar,
+    Union
+)
+
+from db import DB
+from user import User
+
+U = TypeVar(User)
+
+
+def _hash_password(password: str) -> bytes:
+    return bcrypt.hashpw(passwd, bcrypt.gensalt())
